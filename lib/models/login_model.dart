b7955612bea18b4e5176bd0f6305bfd57a08530d
Agyei Caleb Agyemang <calebagyei165@gmail.com>
2024-04-29 17:21:00 +0000

    add toJSON and fromJSON methods to login model

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,6 +1,18 @@
 class LoginModel {
-  int phoneNumber;
-  int stationCode;
+  String phoneNumber;
+  String stationCode;
 
   LoginModel({required this.phoneNumber, required this.stationCode});
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
+        phoneNumber: json['phone_param'],
+        stationCode: json['code_param'],
+      );
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['phone_param'] = phoneNumber;
+    data['code_param'] = stationCode;
+    return data;
+  }
 }
