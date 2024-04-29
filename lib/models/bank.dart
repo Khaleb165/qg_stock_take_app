3489140ab664354b3975fced723511c19c1babc7
Agyei Caleb Agyemang <calebagyei165@gmail.com>
2024-04-29 17:21:32 +0000

    add toJSON and fromJSON methods to bank model

--- lib/models/bank.dart
+++ lib/models/bank.dart
@@ -3,4 +3,14 @@ class Bank {
   String bankName;
 
   Bank({required this.bankId, required this.bankName});
+
+  factory Bank.fromJson(Map<String, dynamic> json) => Bank(
+        bankId: json["bank_id"],
+        bankName: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "bank_id": bankId,
+        "name": bankName,
+      };
 }
